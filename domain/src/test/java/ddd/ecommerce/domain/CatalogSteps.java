5bbe726cef61e7466842e0cb5567f1860babd7e5
Jérémie Grodziski <jeremie@grodziski.com>
2014-04-02 09:40:17 +0200

    fix some stuff

---
+++ domain/src/test/java/ddd/ecommerce/domain/CatalogSteps.java
@@ -0,0 +1,20 @@
+package ddd.ecommerce.domain;
+
+
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Pending;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.Locale;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertNotNull;
+
+public class CatalogSteps {
+
+
+}
