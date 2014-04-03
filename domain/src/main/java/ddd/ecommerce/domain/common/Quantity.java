69f2f742dc2b33aa7c79d96633a97409c9c3845b
El-Arif AHAMADA <elarif.ahamada@gmail.com>
2014-04-03 15:33:06 +0200

    Un stock peut être nul (penser peut etre a typer les quantity)

--- domain/src/main/java/ddd/ecommerce/domain/common/Quantity.java
+++ domain/src/main/java/ddd/ecommerce/domain/common/Quantity.java
@@ -4,15 +4,46 @@ package ddd.ecommerce.domain.common;
  * Created by jeremiegrodziski on 02/04/14.
  */
 public class Quantity {
-    Integer value;
+    private final Integer value;
     public static final Quantity ONE = new Quantity(1);
 
-    public Quantity(Integer value) {
-        if (value < 1) throw new RuntimeException("A quantity is always positive");
+    private Quantity(Integer value) {
+        if (value < 0) throw new RuntimeException("A quantity is always positive");
         this.value = value;
+    }
+    public Quantity valueOf(Integer value){
+        if(value == 1) return ONE;
+        return new Quantity(value);
     }
 
     public Integer getValue() {
         return value;
+    }
+
+    public boolean lt(Quantity quantity){
+        return this.getValue() < quantity.getValue();
+    }
+
+    public boolean lte(Quantity quantity){
+        return this.getValue() <= quantity.getValue();
+    }
+
+    public boolean gt(Quantity quantity){
+        return this.getValue() > quantity.getValue();
+    }
+
+    public boolean gte(Quantity quantity){
+        return this.getValue() >= quantity.getValue();
+    }
+
+    public boolean eq(Quantity quantity){
+        return this.getValue() == quantity.getValue();
+    }
+
+    public Quantity minus(Quantity quantityToRemove) {
+        return new Quantity(this.value-quantityToRemove.value);
+    }
+    public Quantity plus(Quantity quantityToRemove) {
+        return new Quantity(this.value+quantityToRemove.value);
     }
 }
